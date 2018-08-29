6ff7ee6866f4e0d4219bd1df7fadb1d9d658e9d7
programarivm <info@programarivm.com>
2018-08-29 20:34:07 +0100

    testing the svc model

--- src/Babylon.php
+++ src/Babylon.php
@@ -6,7 +6,7 @@ use Phpml\ModelManager;
 
 class Babylon
 {
-    const MODEL_FILEPATH = __DIR__ . '/../models/naive-bayes.txt';
+    const MODEL_FILEPATH = __DIR__ . '/../models/svc.txt';
 
     protected $restoredClassifier;
 
