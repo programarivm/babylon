b4d2000acb80ca4a851beaa91fea866840dc8443
programarivm <info@programarivm.com>
2018-08-30 14:58:59 +0100

    tgl passes the tests

--- tests/unit/BabylonTest.php
+++ tests/unit/BabylonTest.php
@@ -134,6 +134,15 @@ class BabylonTest extends TestCase
         $this->assertEquals('swe', (new Babylon)->detect(Filter::phrase($phrase)));
     }
 
+    /**
+     * @dataProvider tglData
+     * @test
+     */
+    public function detect_tgl($phrase)
+    {
+        $this->assertEquals('tgl', (new Babylon)->detect(Filter::phrase($phrase)));
+    }
+
     public function cesData()
     {
         return [
@@ -406,6 +415,27 @@ class BabylonTest extends TestCase
             ],
             [
                 "Markku bestämde att han inte skulle stå till tjänst nästa gång och inte någon annan gång heller. Te är en populär dryck över hela världen."
+            ],
+        ];
+    }
+
+    public function tglData()
+    {
+        return [
+            [
+                "Alam mo na mahal kita. Ano ito pala ang nata de coco na pinag-uusapan ng lahat. Kung saan may buhay may pag-asa."
+            ],
+            [
+                "May kilala ba kayong isang G. Green? Alam ko ang marikit na kastilyo. Pinakilala niya ako sa kanyang kapatid na lalaki."
+            ],
+            [
+                "Ngayon ang aking ikalabing-anim na kaarawan. Nagpaalam na ang mga bata sa mga matatandang galing sa ibang bayan. Bigyan mo ako ng kalahating kilo ng mansanas."
+            ],
+            [
+                "Hindi posible ang manirahan sa pulong iyan. Wala silang magawa. Alam mo ba iyon? Nahuli siya dahil sa yelo. Napakaimportante ang guni-guni sa araw-araw na buhay."
+            ],
+            [
+                "Talagang hindi gumagana iyan. Kailangan niyang pumunta rito. Hindi ka dapat pumunta sa paaralan. Umihi sa dayaper ang anak mo. Binigay niya sa akin lahat ng pera niya."
             ],
         ];
     }
