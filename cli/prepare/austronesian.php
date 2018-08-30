08477f4cc46d40deaa5732060e2df448e006e9d6
programarivm <info@programarivm.com>
2018-08-30 19:28:42 +0100

    the languages are structured into families

---
+++ cli/prepare/austronesian.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+const DATA_INPUT_FOLDER 	= __DIR__ . '/../../dataset/input/iso-8859/latin/austronesian';
+const DATA_OUTPUT_FOLDER 	= __DIR__ . '/../../dataset/output/iso-8859/latin/austronesian';
+
+require_once 'prepare.php';
---
+++ cli/prepare/germanic.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+const DATA_INPUT_FOLDER 	= __DIR__ . '/../../dataset/input/iso-8859/latin/germanic';
+const DATA_OUTPUT_FOLDER 	= __DIR__ . '/../../dataset/output/iso-8859/latin/germanic';
+
+require_once 'prepare.php';
