facec6526f15a226cb4d9f3a39c8ae032c014542
programarivm <info@programarivm.com>
2018-08-30 19:45:44 +0100

    SVC is removed

--- src/Babylon.php
+++ src/Babylon.php
@@ -6,7 +6,7 @@ use Phpml\ModelManager;
 
 class Babylon
 {
-    const MODEL_FILEPATH = __DIR__ . '/../models/svc.txt';
+    const MODEL_FILEPATH = __DIR__ . '/../models/naive-bayes.txt';
 
     protected $restoredClassifier;
 
