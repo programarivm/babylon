751cac125bb5ca8adf1c2122478b166b813fcf14
programarivm <info@programarivm.com>
2018-08-30 15:22:46 +0100

    pol passes the tests

--- tests/unit/BabylonTest.php
+++ tests/unit/BabylonTest.php
@@ -99,6 +99,15 @@ class BabylonTest extends TestCase
     }
 
     /**
+     * @dataProvider polData
+     * @test
+     */
+    public function detect_pol($phrase)
+    {
+        $this->assertEquals('pol', (new Babylon)->detect(Filter::phrase($phrase)));
+    }
+
+    /**
      * @dataProvider porData
      * @test
      */
@@ -331,6 +340,27 @@ class BabylonTest extends TestCase
             ],
             [
                 "Ik had het gevoel dat ik moest huilen toen ik het nieuws hoorde. Het valt te betwijfelen of deze methode zal werken."
+            ],
+        ];
+    }
+
+    public function polData()
+    {
+        return [
+            [
+                "Po prostu powiedz co mam zrobić. Wolę angielskie samochody od zagranicznych. Proszę najpierw jemu podać posiłek. Anglia pewnie wygra ten mecz. On czyta teraz powieść. Jest mnóstwo rzeczy które powinniśmy przemyśleć."
+            ],
+            [
+                "Jeśli chodzi o Boba to on wszystko przyjmie natomiast Jane jest szalenie ostrożna. Ktoś ich jeszcze używa? Nie będę z tobą więcej rozmawiać ponieważ przeszkadza mi że ciągle flirtujesz z innymi kobietami."
+            ],
+            [
+                "Tom postanowił się nie odzywać dopóki wszyscy pozostali nie wygłoszą swoich opinii. Osobiście uważam że nie ma większej różnicy kto wygra wybory."
+            ],
+            [
+                "Praca w grupach wielosobowych jest strasznie frustrująca. Nie ważne jak bardzo się zestarzeję wciąż będę miał siłę na grę w ping ponga."
+            ],
+            [
+                "On spotyka się ze swoją dziewczyną co sobota. Ten nadzwyczajny wzrost objaśnia się szybkim zjednoczeniem gospodarczym który zaszedł w tym samym czasie."
             ],
         ];
     }
