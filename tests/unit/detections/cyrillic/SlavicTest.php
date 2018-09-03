363b10468ffb31816950ee0ca3124a6843935f00
programarivm <info@programarivm.com>
2018-09-03 23:37:57 +0100

    tests/unit/detections/cyrillic/SlavicTest passes

---
+++ tests/unit/detections/cyrillic/SlavicTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Babylon\Tests\Unit\Detections\Cyrillic;
+
+use Babylon\Detector\FamilyDetector;
+use Babylon\Detector\LanguageDetector;
+use PHPUnit\Framework\TestCase;
+
+class SlavicTest extends TestCase
+{
+    /**
+     * @dataProvider bulData
+     * @test
+     */
+    public function family_detect_bul($text)
+    {
+        $this->assertEquals('slavic', (new FamilyDetector($text))->detect());
+    }
+
+    /**
+     * @dataProvider rusData
+     * @test
+     */
+    public function family_detect_rus($text)
+    {
+        $this->assertEquals('slavic', (new FamilyDetector($text))->detect());
+    }
+
+    public function bulData()
+    {
+        return [
+            [
+                'И ето и до днес кръвта на тези мъченици не е още засъхнала по бащините ни
+                полета; сенките им бродят денем и нощем и чакат онези, кои от Петрушан им
+                пожелаха "добър час и добра стига!'
+            ],
+        ];
+    }
+
+    public function rusData()
+    {
+        return [
+            [
+                'Женщине тут же мудрый обычай говорит: знай только три вещи: кухню,
+                постель, а по воскресеньям—церковь. Счастливая Австралия кроме этик
+                трех позволенных вещей дала женщине и четвертую—право голоса.'
+            ],
+        ];
+    }
+}
