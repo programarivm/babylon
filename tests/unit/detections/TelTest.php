51262c2d5728b9ce05f0d819eea4c5c7fd97bbb0
Jordi Bassagañas <info@programarivm.com>
2018-09-04 00:42:30 +0100

    Babylon is taught the Telugu language (#19)

---
+++ tests/unit/detections/TelTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Babylon\Tests\Unit\Detections;
+
+use Babylon\Detector\LanguageDetector;
+use PHPUnit\Framework\TestCase;
+
+class TelTest extends TestCase
+{
+    /**
+     * @dataProvider telData
+     * @test
+     */
+    public function language_detect_tel($text)
+    {
+        $this->assertEquals('tel', (new LanguageDetector($text))->detect());
+    }
+
+    public function telData()
+    {
+        return [
+            [
+                "తన కొడుకు చదువు కుదుటపడడం, ఎవ్వరూ వంటిమీద ఏ పెట్రోలో పోసి అగ్గిపుల్ల గియ్యకపోవడం తప్ప మరో ఆలోచనలేదు భద్రకి."
+            ],
+        ];
+    }
+}
