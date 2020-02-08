d34bbc10eb16ddda1b537a0a8380172313081c54
programarivm <info@programarivm.com>
2020-02-08 10:35:19 +0000

    an array of patterns is used in the filter's regular expression

--- src/Filter.php
+++ src/Filter.php
@@ -7,16 +7,21 @@ class Filter
     public static function text(string $text): string
     {
         $text = mb_strtolower($text);
-        $text = preg_replace('/\'/', ' ', $text);
-        $text = preg_replace('/(“|”)/', ' ', $text);
-        $text = preg_replace('/(«|»)/', ' ', $text);
-        $text = preg_replace('/(\"|\")/', ' ', $text);
-        $text = preg_replace('/’/', ' ', $text);
-        $text = preg_replace('/…/', ' ', $text);
-        $text = preg_replace('/[[:punct:]]/', ' ', $text);
-        $text = preg_replace('#\p{Pd}#u', ' ', $text);
-        $text = preg_replace('/[0-9]+/', ' ', $text);
-        $text = preg_replace('!\s+!', ' ', $text);
+
+        $patterns = [
+            '/\'/',
+            '/(“|”)/',
+            '/(«|»)/',
+            '/(\"|\")/',
+            '/’/',
+            '/…/',
+            '/[[:punct:]]/',
+            '#\p{Pd}#u',
+            '/[0-9]+/',
+            '!\s+!',
+        ];
+
+        $text = preg_replace($patterns, ' ', $text);
 
         return $text;
     }
